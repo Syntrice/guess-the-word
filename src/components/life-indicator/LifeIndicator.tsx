0087faad5a62cddd573096cad27941b81a7eda49
Syntrice <syntrice@protonmail.com>
2025-03-29 21:51:11 +0000

    feat: Make life icons response
    Use smaller Heart icon font size on very small displays

--- src/components/life-indicator/LifeIndicator.tsx
+++ src/components/life-indicator/LifeIndicator.tsx
@@ -9,7 +9,7 @@ export default function LifeIndicator({ lives } : LifeIndicatorProps) {
         <ul className="flex justify-center gap-5">
             {[...Array(lives)].map((_, index) => (
                 <li key={index}>
-                    <Icon icon="fluent-emoji-flat:red-heart" className="text-4xl" />
+                    <Icon icon="fluent-emoji-flat:red-heart" className="text-4xl max-sm:text-2xl" />
                 </li>
             ))}
         </ul>
